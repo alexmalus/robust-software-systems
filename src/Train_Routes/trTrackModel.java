800842ab9dff77a2b39ce6457e5a21a8262a46d6
Esbern <esbern_a-h@hotmail.com>
2015-03-18 21:48:50 +0100

    read-in of route specification.

---
+++ src/Train_Routes/trTrackModel.java
@@ -0,0 +1,41 @@
+package Train_Routes;
+
+public class trTrackModel {
+	private char id;
+	private boolean isStop;
+	private trTrackModel leftConnection;
+	private trTrackModel rightConnection;
+	
+	public trTrackModel(char id){
+		this.id = id;
+		this.isStop = false;
+	}
+	
+	public char getId(){
+		return this.id;
+	}
+	
+	public boolean getIsStop(){
+		return this.isStop;
+	}
+	
+	public void setIsStop(boolean isStop){
+		this.isStop = isStop;
+	}
+	
+	public trTrackModel getLeftConnection(){
+		return this.leftConnection;
+	}
+	
+	public void setLeftConnection(trTrackModel leftConenction){
+		this.leftConnection = leftConenction;
+	}
+	
+	public trTrackModel getRightConnection(){
+		return this.rightConnection;
+	}
+	
+	public void setRightConnection(trTrackModel rightConnection){
+		this.rightConnection = rightConnection;
+	}
+}
