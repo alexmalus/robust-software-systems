2dc32c44a4225b4249da2e4d4007f3d680e8c423
Alexandru Malus <malus.alexandru@yahoo.com>
2015-04-09 12:11:27 +0200

    Main for you

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import Railway_Networks.RailwayParser;
+
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		RailwayParser rp = new RailwayParser();
+		rp.Run("train.txt");
+		//rp.Run("test_cases\\railway\\railway_1.txt");
+	}
+
+}
